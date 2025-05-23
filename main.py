7a4d76e4aee31871d0bebb5b52a162b5694cb28e
Gava95 <valentin.gacoin@hotmail.com>
2025-05-23 18:43:59 +0200

    add constants.py and update main.py (black screen window)

--- main.py
+++ main.py
@@ -2,9 +2,26 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+from constants import *
 
 def main():
-	print("Starting Asteroids!")
+    # Initiliaze pygame package
+    pygame.init()
 
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    # Create a new GUI window
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        pygame.Surface.fill(screen, (0,0,0))
+        pygame.display.flip()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+    
 if __name__ == "__main__":
     main()
